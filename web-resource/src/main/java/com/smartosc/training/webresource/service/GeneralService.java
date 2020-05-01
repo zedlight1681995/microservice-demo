4d46472b4ae0e13812b55bbcebed19e4cf567a26
datntse03734 <datntse03734@gmail.com>
2020-05-02 00:56:46 +0700

    hateoas v2

--- web-resource/src/main/java/com/smartosc/training/webresource/service/GeneralService.java
+++ web-resource/src/main/java/com/smartosc/training/webresource/service/GeneralService.java
@@ -5,7 +5,11 @@ import com.smartosc.training.webresource.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface GeneralService<T> {
+
+    List<T> findAll();
 
     Page<T> findAll(Pageable pageable);
 
